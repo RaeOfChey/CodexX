ea25baf746da26fdb9fba3109b4d987751aadce4
RaeofChey <cheyennaraelynn@gmail.com>
2024-11-15 15:36:01 -0600

    adjusted connection.ts + added db url

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -2,17 +2,22 @@ import dotenv from 'dotenv';
 dotenv.config(); // Load environment variables from .env
 
 // Verify if environment variables are loaded correctly
-console.log('dotenv loaded:', process.env.DB_USER);
-console.log('DB_USER:', process.env.DB_USER);  // Should output 'postgres'
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);  // Should output 'Uraqt1006!'
-console.log('DB_NAME:', process.env.DB_NAME);  // Should output 'videogame_db'
+console.log('dotenv loaded:', process.env.DATABASE_URL);
 
 // Sequelize setup
 import { Sequelize } from 'sequelize';
 
-// Use DB_URL if it exists (for Render database)
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+// Use DATABASE_URL if it exists (for Render database)
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true, // Ensure SSL is enabled
+          rejectUnauthorized: false, // This is necessary for Render databases
+        },
+      },
+    })
   : new Sequelize(
       process.env.DB_NAME || '',
       process.env.DB_USER || '',
