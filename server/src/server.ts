a5be22948041a177685a52d47cb4954b18b90ea7
RaeofChey <cheyennaraelynn@gmail.com>
2024-11-15 14:32:27 -0600

    Adjusted server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 const forceDatabaseRefresh = false;
 
+import path from 'path';
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -7,12 +9,20 @@ import express from 'express';
 import routes from './routes/index.js';
 import sequelize  from './config/connection.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use('/api', routes);
 app.use(express.static('../client/dist'));
+
+// Catch-all route to serve React's index.html for unrecognized routes
+app.get('*', (__req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
