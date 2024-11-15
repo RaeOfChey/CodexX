1ca68c60589c32dcafe210036a28c87ebe8fd1cc
RaeofChey <cheyennaraelynn@gmail.com>
2024-11-15 13:14:05 -0600

    Installed

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,17 +7,12 @@ import express from 'express';
 import routes from './routes/index.js';
 import sequelize  from './config/connection.js';
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-
 app.use(express.json());
 app.use('/api', routes);
-
 app.use(express.static('../client/dist'));
-
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
