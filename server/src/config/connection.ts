f3f9ed9817b35da7b27bbd94885b5c959ae9d362
RaeofChey <cheyennaraelynn@gmail.com>
2024-11-15 14:08:02 -0600

    DB

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,14 +1,16 @@
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config(); // Load environment variables from .env
 
+// Verify if environment variables are loaded correctly
 console.log('dotenv loaded:', process.env.DB_USER);
-
 console.log('DB_USER:', process.env.DB_USER);  // Should output 'postgres'
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD);  // Should output 'Uraqt1006!'
 console.log('DB_NAME:', process.env.DB_NAME);  // Should output 'videogame_db'
 
+// Sequelize setup
 import { Sequelize } from 'sequelize';
 
+// Use DB_URL if it exists (for Render database)
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(
